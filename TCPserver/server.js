8b64bcfdf6bca43ad503c60693882a7427a515c7
clareluna <clarelunamonahan@gmail.com>
2015-09-16 20:51:46 -0700

    testing a go

--- TCPserver/server.js
+++ TCPserver/server.js
@@ -2,23 +2,30 @@
 
 var net = require('net');
 var fs = require('fs');
-var name = require('gen-id');
+var name = require('dog-names');
 var request = '';
 
-var server	= net.createServer(function (socket) {
-socket.on('data', function(data) {
-	request = name.get();
-	fs.writeFile(request + '.txt', data, function(err) {
-		if (err) {
-			return err;
-		} else {
-			console.log('file saved as ' + request +'.txt');
+console.log(request);
+
+var server = net.createServer(function(socket) {
+	socket.on('data', function(data) {
+		console.log(data.toString());
+		request = name.femaleRandom();
+		console.log(request);
+		fs.writeFile(__dirname + '/../files/' + request + '.txt', data, function(err) {
+			if (err) {
+				console.log('this is the error: ' + err);
+			} else {
+				console.log('file saved as ' + request +'.txt');
 			}
+			socket.end();
+		// socket.on('end', function() {
+		// 	console.log('disconnected');
+		// 	});
 		});
-	});
-});
+	});	
+});	
 
 server.listen(3000, function() {
 	console.log('server running on port 3000');
 });
-
--- test.js
+++ test.js
@@ -1,19 +1,34 @@
 'use strict';
 
 var chai = require('chai');
-var expect = require('chai').expect;
-var tcp = require('./TCPserver/server.js');
+var http = require('http'); //needed?
+var expect = chai.expect;
+var fs = require('fs');
+require('./TCPserver/server.js');
 var net = require('net');
 var chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
 
-describe('tcp server should log unique names from gen-id', function(){
-	before(function(){
-		tcp;
+
+describe('tcp server should log unique names from random female dog name', function(){
+	var preTestDir;
+	before(function(done) {
+		preTestDir = fs.readdirSync(__dirname + '/files');
+		console.log(preTestDir);
+		console.log((preTestDir).length);
+		chai.request('localhost:3000')
+			.get('/')
+			// .send({name: 'testing'})
+			.end(function(err, res){
+			if (err) console.log('before in describe block err: ' +err);
+			done();
+			});
 	});
-	it('should write new file name', function(done) {
-		chai.request('/') 
-			.get('/'); 
-		var filename = __dirname + './../' + request +'.txt';
-		expect(filename).to.exist;
+	
+	it('should write new file name', function() {
+		var postTestDir = fs.readdirSync(__dirname + '/files');
+		console.log(postTestDir);
+		console.log((postTestDir).length);
+		expect(postTestDir.length).to.be.above(preTestDir.length);
 	});
 });
